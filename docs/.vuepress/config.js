fa83fe0ac02d2b4791d2ef277de2b6dab9a5e669
joey <2390923149@qq.com>
2022-10-31 11:46:50 +0800

    feat: 添加logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   title: '狗子的博客',
   description: '记录生活，记录bug!',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png'} ]
+  ],
   themeConfig: {
+    logo: '/logo.png',
     sidebar: 'auto',
     lastUpdated: 'Last Updated'
   },
