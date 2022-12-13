ff9392ca9094093c56c699e9ea3e38ec56ea2308
joey <2390923149@qq.com>
2022-12-13 09:47:03 +0800

    feat: 表格v1

---
+++ docs/.vuepress/components/canvas-sheet-table.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <canvas id="canvas-sheet"></canvas>
+  </div>
+</template>
+
+<script>
+import { Sheet } from './Sheet'
+
+export default {
+  mounted() {
+    const sheet = new Sheet('#canvas-sheet', {
+      width: 800,
+      height: 500,
+      cellWidth: 100,
+      cellHeight: 30,
+      scrollBar: true
+    })
+
+    sheet.draw()
+    window.sheet = sheet
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+#canvas {
+  border: 1px solid #999;
+  margin: 20px;
+}
+</style>
